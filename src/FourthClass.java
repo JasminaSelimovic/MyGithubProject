d3beba5f7a3a1f1982295f8ca26012c53c84b6a2
jasmina <jasmina_s4@hotmail.com>
2022-07-07 21:28:17 -0400

    MyGithubProject

--- src/FourthClass.java
+++ src/FourthClass.java
@@ -1,2 +1,6 @@
 public class FourthClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+    }
 }
--- src/ThirdClass.java
+++ src/ThirdClass.java
@@ -1,2 +1,6 @@
 public class ThirdClass {
+
+    public static void main(String[] args) {
+        System.out.println("Jasmina");
+    }
 }
